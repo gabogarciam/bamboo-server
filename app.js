fb46d747538534ce58643a838d4c70c3922989cb
Gabriel Garcia <gabogarciam71@gmail.com>
2018-08-03 15:26:51 +0200

    errors handlers modified and route /me done

--- app.js
+++ app.js
@@ -44,23 +44,18 @@ app.use(session({
 // -- Routes -- //
 app.use('/auth', auth);
 
-// -- 404 and error handler
-
-// NOTE: requires a views/not-found.ejs template
+/// catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.status(404);
-  res.render('not-found');
+  res.status(404).json({code: 'not found'});
 });
 
-// NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
   // always log the error
   console.error('ERROR', req.method, req.path, err);
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
-    res.status(500);
-    res.render('error');
+    res.status(500).json({code: 'unexpected'});
   }
 });
 
